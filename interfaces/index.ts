d5e4ae002cdc91b07b8dbff87c7ac346c4b369ed
AnnaSv <anna.sviatelyk@gmail.com>
2020-09-02 16:20:16 +0400

    project init

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,9 +2,9 @@
 // and then use them in any component by importing them. For
 // example, to import the interface below do:
 //
-// import User from 'path/to/interfaces';
+// import Post from 'path/to/interfaces';
 
-export type User = {
-  id: number
-  name: string
-}
+export type Post = {
+  id: number;
+  name: string;
+};
