11aadd9068e2599921693db81d4d3e1693a4513d
AnnaSv <anna.sviatelyk@gmail.com>
2020-09-05 12:19:19 +0400

    store refactored to ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { MakeStore, createWrapper, Context, HYDRATE } from 'next-redux-wrapper';
-import { State } from '../interfaces';
+import { createWrapper } from 'next-redux-wrapper';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
-const initialState = {
-    items: [],
-    error: null,
-};
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-export const makeStore = (context) => createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+export const makeStore = () => createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export const wrapper = createWrapper(makeStore, { debug: true });
